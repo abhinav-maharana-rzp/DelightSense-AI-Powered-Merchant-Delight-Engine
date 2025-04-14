4923fe1833355feb641ded1937bc5c2e3ed3b3cc
abhinav-maharana-rzp <abhinav.maharana@razorpay.com>
2025-04-14 11:28:33 +0530

    frontend setup

---
+++ apps/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+      <button className="bg-black text-white p-6 rounded-lg">Click me</button>
+    </div>
+    </div>
+  )
+}
+
+export default App
